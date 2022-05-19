fe096a76d530af2948c61d0d1612db16d6d3e168
Raissa Martins <raissamartinsarq@gmail.com>
2022-05-19 20:42:11 -0300

    Documentação Swagger

--- src/main/java/com/cat/api/controller/ApiApplicationController.java
+++ src/main/java/com/cat/api/controller/ApiApplicationController.java
@@ -2,13 +2,11 @@ package com.cat.api.controller;
 
 import java.util.Arrays;
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import com.cat.api.TheCatCategories;
 import com.cat.api.TheCatImages;
 
